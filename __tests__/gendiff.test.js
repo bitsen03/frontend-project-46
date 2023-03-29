06cee92d1ddea49771c6cab1ff67d0733c7fa319
bitsen <bitsen03@mail.ru>
2023-03-29 10:32:06 +0300

    step 8

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,16 +3,17 @@ import {readFileSync} from 'fs';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff', () => {
-    const filepath1 = getFixturePath('file1.json')
-    const filepath2 = getFixturePath('file2.json')
-    const result = genDiff(filepath1,filepath2)
-    const expected =  readFile('fileTest.json');
+    const filepath1 = getFixturePath('plainFile1.yml')
+    const filepath2 = getFixturePath('plainFile2.yml')
+    const result = genDiff(filepath1,filepath2, 'plain')
+    const expected =  readFile('plainExpected.yml');
     expect(result).toEqual(expected)
 });
 test('gendiff1', () => {
@@ -20,5 +21,12 @@ test('gendiff1', () => {
     const filepath2 = getFixturePath('file2.yml')
     const result = genDiff(filepath1,filepath2)
     const expected =  readFile('fileTest.json');
+    expect(result).toEqual(expected)
+});
+test('gendiff', () => {
+    const filepath1 = getFixturePath('file1.json')
+    const filepath2 = getFixturePath('file2.json')
+    const result = genDiff(filepath1,filepath2, 'json')
+    const expected =  readFile('jsonExpected.json');
     expect(result).toEqual(expected)
 });
